f4fe6d1d512834c529a356d0156a5ff1e94541fa
Iago Menezes <iagomenezes555@gmail.com>
2025-07-09 15:38:13 -0300

    refac: cards with for

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -9,15 +9,28 @@ import { MatCardModule } from '@angular/material/card';
 })
 export class CardComponent {
 
-  todo() {
-    window.open("https://github.com/IagoMenezes555/To-Do-List", "_blank");
-  }
+  projetos = [
+    {
+      nome: "To-Do List",
+      descricao: "To-Do List é um projeto simples para organizar tarefas. Nele, o usuário pode adicionar, marcar como concluídas e excluir tarefas. Foi essencial para eu aprender e praticar o uso do localStorage em JavaScript.",
+      link: "https://github.com/IagoMenezes555/To-Do-List"
+    },
+    {
+      nome: "Urna Eletrônica",
+      descricao: "Urna Eletrônica é um projeto feito em Angular que simula uma votação. Com ele, é possível digitar o número do candidato, confirmar, corrigir ou votar em branco. Foi importante para eu praticar componentização, serviços e manipulação de dados no Angular.",
+      link: "https://github.com/IagoMenezes555/Urna-7"
+    },
+    {
+      nome: "Estoque",
+      descricao: "Estoque é um projeto voltado para monitorar e exibir os itens disponíveis no estoque de uma empresa. O objetivo é facilitar a organização e o gerenciamento eficiente dos produtos.",
+      link: "https://github.com/IagoMenezes555/APP-de-Estoque"
+    }
+  ];
 
-  urna(){
-    window.open("https://github.com/IagoMenezes555/Urna-7", "_blank");
-  }
-
-  estoque(){
-    window.open("https://github.com/IagoMenezes555/APP-de-Estoque", "_blank");
+  abrirProjeto(nome: string) {
+    const projeto = this.projetos.find(p => p.nome === nome);
+    if (projeto) {
+      window.open(projeto.link, "_blank");
+    }
   }
 }
